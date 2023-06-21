1c875480d2800dded53903a96b69d6823d9b914e
Daniella Omenogor <daniellaomenogor@gmail.com>
2023-06-21 20:10:28 +0000

    add routes for html pages

--- app.py
+++ app.py
@@ -8,5 +8,17 @@ def home():
     # return "Hello world"
     return render_template('index.html')
 
+@app.route('/explore')
+def explore():
+    return render_template('explore.html')
+
+@app.route('/discounts')
+def discounts():
+    return render_template('discounts.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
 if __name__ == '__main__':
     app.run()
